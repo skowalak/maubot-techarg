ae88a27bc68745737e568abfc75024a68f826da0
Sebastian Kowalak <sebastian@skowalak.de>
2021-11-19 11:18:30 +0100

    Fix confusing terminology

--- techarg.py
+++ techarg.py
@@ -1,24 +1,24 @@
 from maubot import Plugin, MessageEvent
 from maubot.handlers import event, command
 from mautrix.types 
-from .reasons import strings_arg, strings_noarg
+from .excuses import strings_arg, strings_noarg
 
 import random
 
 
 class TechargBot(Plugin):
-    @command.new("ta", help="Get reasons why it just won't work. Syntax: !ta <term>")
+    @command.new("ta", help="Get excuses why it just won't work. Syntax: !ta <term>")
     @command.argument("term", pass_raw=True, required=False)
     async def handler(self, event: MessageEvent, term: str) -> None:
 
         if term:
             # pick string with an argument
-            reason = random.choice(strings_arg)
-            reason.format(term=term)
+            excuse = random.choice(strings_arg)
+            excuse = excuse.format(term=term)
 
         else:
-            # no string specified, get reason without argument
-            reason = random.choice(strings_noarg)
+            # no term specified, get string without argument
+            excuse = random.choice(strings_noarg)
 
-        await event.respond(reason)
+        await event.respond(excuse)
     
